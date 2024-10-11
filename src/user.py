8a84c0b123284ee7383afb1af10d76a1b8fa62b7
kavya-chopra <kavyachopra292002@gmail.com>
2024-10-12 00:50:14 +0100

    Adding record class for user assets

--- src/user.py
+++ src/user.py
@@ -2,7 +2,7 @@ import uuid
 
 class User:
 
-    def __init__(self, id:uuid, funds:int, assets:dict[Asset, int]):
+    def __init__(self, id:uuid, funds:int, assets:list[UserAsset]):
         self.id = uuid.uuid1()
         self.funds = funds
         self.assets = assets
